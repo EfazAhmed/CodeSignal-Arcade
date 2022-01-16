df92e8fbc38803f1f099922b9d108d19b89e3aef
EfazAhmed <73269488+EfazAhmed@users.noreply.github.com>
2022-01-16 18:55:53 -0500

    added the core 49

---
+++ The Core/6_weak_numbers.py/49_rectangle_rotation.py
@@ -0,0 +1,11 @@
+def solution(a, b):
+    count = 0
+    for i in range(-(a+100), a+111):
+        for j in range(-(b+100), b+111):
+            x = (i+j)/math.sqrt(2)
+            y = (j-i)/math.sqrt(2)
+            
+            if x < a/2 and x > -a/2:
+                if y < b/2 and y > -b/2:
+                    count += 1
+    return count
