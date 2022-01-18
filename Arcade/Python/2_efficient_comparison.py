e335111b5f069e2374a341094ede8ebfacaae732
EfazAhmed <73269488+EfazAhmed@users.noreply.github.com>
2022-01-17 19:25:21 -0500

    Added Python 2

---
+++ Arcade/Python/2_efficient_comparison.py
@@ -0,0 +1,5 @@
+# You would like to write a function that takes integer numbers
+# x, y, L, and R as parameters, and returns True if x^y lies within the
+# interval (L, R] and False otherwise.
+
+# Option 1 is the most optimal => (if L < x ** y <= R:)
