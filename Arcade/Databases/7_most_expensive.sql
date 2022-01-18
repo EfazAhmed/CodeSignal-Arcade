6c0fcac8bf2a1ee831c69be719c063e8461ed25a
EfazAhmed <73269488+EfazAhmed@users.noreply.github.com>
2022-01-17 20:09:54 -0500

    Added database 7

---
+++ Arcade/Databases/7_most_expensive.sql
@@ -0,0 +1,7 @@
+CREATE PROCEDURE solution()
+BEGIN
+	/* Write your SQL here. Terminate each statement with a semicolon. */
+    SELECT name FROM Products
+    ORDER BY price * quantity DESC, name LIMIT 1;
+
+END
